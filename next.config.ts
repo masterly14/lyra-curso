b68a99bf89b85112e60df5dd809af74351f95057
masterly14 <svaron066@gmail.com>
2025-11-02 05:46:09 -0500

    first_commit

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Omitir errores de ESLint en construcciones de producción
-  experimental: {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  /* config options here */
 };
 
 export default nextConfig;
